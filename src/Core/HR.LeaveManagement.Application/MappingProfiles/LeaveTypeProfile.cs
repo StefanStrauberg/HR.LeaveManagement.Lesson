16f19878c017fb0a29d5b58da71bbb09f6cd8af2
StefanStrauberg <stefanstrauberg@gmail.com>
2023-10-11 21:45:16 +0300

    Adding & base configuration Persistence layer

--- src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.MappingProfiles;
@@ -9,5 +10,6 @@ internal class LeaveTypeProfile : Profile
     public LeaveTypeProfile()
     {
         CreateMap<LeaveType, LeaveTypeDto>();
+        CreateMap<LeaveType, LeaveTypeDetailsDto>();
     }
 }
---
+++ src/Infrastructure/HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext;
+
+internal sealed class HrDatabaseContext : DbContext
+{
+    public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+    {
+    }
+
+    public DbSet<LeaveType>? LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation>? LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest>? LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>().Where(x => x.State is EntityState.Added or EntityState.Modified))
+        {
+            entry.Entity.DateUpdated = DateTime.Now;
+            if (entry.State == EntityState.Added)
+                entry.Entity.DateCreated = DateTime.Now;
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
